df7ecc50620d14aaa2102e5727efd88964b3e691
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-19 16:11:08 +0100

    fixed activeFunc code ,Milestone 2 completed

--- milestone1/assets/js/main.js
+++ milestone1/assets/js/main.js
@@ -3,26 +3,7 @@ let app = new Vue ({
     data: {
         userImg: "./assets/img/avatar_2.jpg",
         myName: "Antonio",
-        activeContacts: [
-            {
-            name: "Andrea",
-            avatar: "./assets/img/avatar_8.jpg",
-            
-            visible: true,
-            messages: [
-                {
-                    date: dayjs().format("H:mm"),
-                    text: "How do you feel carring the world on your shoulder?",
-                    status: "sent"
-                },
-                {
-                    date: dayjs().format("H:mm"),
-                    text: "I feel like always mate",
-                    status: "received"
-                }
-            ]
-         
-        }],
+        activeContacts: [],
         contacts: [ 
             {
                 name: "Roberto",
@@ -181,8 +162,8 @@ let app = new Vue ({
     },
     methods: {
         activeFunc(i){
-            let act = this.contacts.splice(i,1);
-            this.activeContacts.unshift(act[i]);           
+            let act = this.contacts[i];
+            this.activeContacts.unshift(act);           
         }
     }
 })
